3c93f3cbb6f2f3407addc7ef479957b07e09f307
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-13 15:43:51 +0100

    Changed the File Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ThemeProvider, CSSReset } from '@chakra-ui/react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ThemeProvider>
+     <CSSReset/>
+     <h1 style={{color:'red', fontSize:200, textAlign:'center'}}>Wenovate</h1>
+     <h1 style={{color:'blue', fontSize:100, textAlign:'center'}}>Damilare Start Working!!</h1>
+
+   </ThemeProvider>
   );
 }
 
