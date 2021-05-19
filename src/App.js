e712826a8e2e933e9810367bead19292b692f14a
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-05-19 14:30:04 +0100

    a

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { ThemeProvider, CSSReset } from '@chakra-ui/react'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Body from './components/Body';
-import BasicUsage from './components/Modal';
 
 
 function App() {
   return (
    <ThemeProvider>
      <CSSReset/>
-     <BasicUsage/>
      <Navbar/>
       <Body/>
     <Footer />
