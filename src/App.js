fa354a7de27b3a1e49a109b3b947a63a593174cf
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-19 12:28:10 +0100

    Connect Landing Componetsadded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Storyboard from './components/Storyboard'
+import Connect from './components/Connect'
 import { Jumbotron } from 'react-bootstrap';
 function App() {
   return (
+    <>
     <Jumbotron className='storyboard-holder'>
     <Storyboard/>
     </Jumbotron>
+    <Connect/>
+    </>
   );
 }
 
