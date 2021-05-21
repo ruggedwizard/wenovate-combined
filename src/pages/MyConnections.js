4d53c4b08c965faffb35b2ef44a3e39893bb6e50
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-05-21 17:17:27 +0100

    Connect sub-component updated

---
+++ src/pages/MyConnections.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Button, Flex, Grid, Spacer, Text } from "@chakra-ui/react"
+
+const MyConnections = () => {
+    return (
+        <Grid templateColumns="repeat(3, 1fr)" gap={50}>
+            
+       
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Sale</Text>
+                <Spacer />
+                <Text color="red">00</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Start</Button>
+        </Flex>
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Discussions</Text>
+                <Spacer />
+                <Text color="#68F195">99+</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Learn</Button>
+        </Flex>
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Sale</Text>
+                <Spacer />
+                <Text color="#68F195">99+</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Start</Button>
+        </Flex>
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Sale</Text>
+                <Spacer />
+                <Text color="#68F195">99+</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Start</Button>
+        </Flex>
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Sale</Text>
+                <Spacer />
+                <Text color="#68F195">99+</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Start</Button>
+        </Flex>
+        <Flex flexDirection="column" borderLeft="1px solid #ddd" borderTop="1px solid #ddd" boxShadow="5px 5px 2px #ddd" h={220} w={250} p={20}>
+            <Flex fontSize={20}>
+                <Text fontWeight="bold" color="#070941">Sale</Text>
+                <Spacer />
+                <Text color="#68F195">99+</Text>
+            </Flex>
+                <Spacer />
+            <Text>When you male sales, you can keep track pf then by clicking this card</Text>
+                <Spacer />
+            <Button w={130} p={6} color="#fff" bg="#070941">Start</Button>
+        </Flex>
+
+        </Grid>
+    )
+}
+
+export default MyConnections
