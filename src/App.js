bd2dbcf7bead08b303f47729ecb5aeeedfdef35a
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-21 08:02:10 +0100

    navbar component updated with login and register modal

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Body from './components/Body';
 import Bodynew from './components/Bodynew'
 import Loginmodal from './components/Loginmodal';
 import Registermodal from './components/Registermodal';
+import NavLinks from './components/NavLinks';
 
 
 function App() {
   return (
     <>
+    <NavLinks/>
     <Bodynew/>  
     <Loginmodal/>
     <Registermodal/>
--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import {
-    Box,
-    Button,
-    Flex,
-    Spacer,
-    Text,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Lorem
-  } from "@chakra-ui/react"
- 
+import { Flex, Spacer, Text, useDisclosure } from "@chakra-ui/react"
+import {Button} from 'react-bootstrap' 
+import Loginmodal from './Loginmodal'
+import Registermodal  from './Registermodal'
 
 const NavLinks = () =>{
-    // const { isOpen, onOpen, onClose } = useDisclosure()
+   
     const { isOpen, onOpen, onClose } = useDisclosure()
     const finalRef = React.useRef();
     return(
@@ -27,41 +14,19 @@ const NavLinks = () =>{
                     <Spacer />
                     <div style={{padding:'10px', marginTop:'7px', border:"5px solid #070941", height:"35px", width:"35px", borderRadius:'50%', }}></div>
                     <Spacer />
-                    <Text padding='10px'><Button >Features</Button></Text>
+                    <Text padding='10px'>Features</Text>
                     <Spacer />
-                    <Text padding='10px'><Button >Blog</Button></Text>
+                    <Text padding='10px'>Blog</Text>
                     <Spacer />
-                    <Text padding='10px'><Button >Connect</Button></Text>
+                    <Text padding='10px'>Connect</Text>
                     <Spacer />
-                    <Text padding='10px 15px' bg="#68F195" color='#070941'><Button onClick={onOpen}>Sign in</Button></Text>
+                    <Text  ><Loginmodal/></Text>
                     <Spacer />
-                    <Text padding='10px 15px' bg="#070941" color="#fff" ml={30}><Button onClick={onOpen}>Register</Button></Text>
+                    <Text   ml={30}><Registermodal/></Text>
                     <Spacer />
                 </Flex>
-
-
-                
- 
-      {/* <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Lorem count={2} />
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal> */}
-        </>
+          
+     </>
     )
 }
 
