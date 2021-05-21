1a80c0145e4ef3fe075f0e364f3f14a82487176e
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-21 15:48:33 +0100

    Carousel Features Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import { ThemeProvider, CSSReset } from '@chakra-ui/react'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Body from './components/Body';
 import Bodynew from './components/Bodynew'
-import Loginmodal from './components/Loginmodal';
-import Registermodal from './components/Registermodal';
 import NavLinks from './components/NavLinks';
 
 
--- src/components/Bodynew.js
+++ src/components/Bodynew.js
@@ -1,11 +1,64 @@
 import React from 'react'
-import { Container, Image } from 'react-bootstrap'
+import { Container, Image, Carousel } from 'react-bootstrap'
 import salesImg from '../assets/sales.jpg'
 import farmers from '../assets/farmerstalk.jpg'
+import farmertwo from '../assets/farmer3.jpg'
 const Bodynew = ()=>{
     return (
         <div>
             <Container>
+                <Carousel>
+                    <Carousel.Item>
+                        <img className='d-block w-100 carousel-image' src={farmers} alt={farmers} style={{opacity:0.5}} />
+                        <Carousel.Caption>
+                        <div className='two-sided-text' >
+                            <div>
+                             <strong>connect, make sales and sell</strong>
+                            <h1>
+                                in the <span className='green-text'>best </span> online <br/>
+                                Community for <span className='green-text'>farmers</span>
+                            </h1>
+                            <h2>access quality information</h2>
+                            <p>about farming best practice to boost your farm production</p>
+                            </div>
+                         </div>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={salesImg} alt={salesImg} className='d-block w-100 carousel-image' style={{opacity:0.5}} />
+                    <Carousel.Caption>
+                        <div className=' two-sided-text' >
+                            <div>
+                            <h1>
+                                A Great online <span className='green-text'> Community</span> to <br/>
+                                make Sales as <span className='green-text'>farmers</span>
+                            </h1>
+                                <h2>access a Larger Market</h2>
+                                <p>Import and Export Made Easy for Local Farmers</p>
+                            </div>
+                        </div>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={farmertwo} alt={farmertwo} className='d-block w-100 carousel-image'style={{opacity:0.5}}/>
+                        <Carousel.Caption>
+                        <div className='two-sided-text' >
+                        <div>
+                         <strong>connect, make sales and sell</strong>
+                        <h1>
+                            in the <span className='green-text'>best </span> online <br/>
+                            Community for <span className='green-text'>farmers</span>
+                        </h1>
+                            <h2>access quality information</h2>
+                            <p>about farming best practice to boost your farm production</p>
+                        </div>
+                        </div>
+                    </Carousel.Caption>
+                    </Carousel.Item>
+                    
+                </Carousel>
+
+
                 <div className='jumbotron two-sided-text' >
                 <div>
                 <strong>connect, make sales and sell</strong>
@@ -29,15 +82,15 @@ const Bodynew = ()=>{
                     </div>
                 </div>
                 <div className='sales-story container'>
-                    <h2 style={{textAlign:'center'}}>How to make sales</h2>
+                    <h2 style={{textAlign:'center'}}>How We Operate</h2>
                     <strong>Step 1</strong>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odit, sit laborum suscipit consectetur dolorem voluptatum similique totam voluptatibus reiciendis. </p>
+                    <p>Don't Have an Account Register, Already Have an Account Login</p>
                     <strong>Step 2</strong>
-                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed corrupti eaque voluptatum! Dicta atque pariatur qui magnam nulla soluta animi. </p>
+                    <p> Meet With Experts Like You Share Ideas on How to Make better produce </p>
                     <strong>Step 3</strong>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt provident repellendus voluptatum qui velit reprehenderit nemo quasi consequatur. Voluptates ullam quae officiis sed minima provident. </p>
+                    <p> Having Financial Issues? We Connect You with the Right Financial Institutions to Secure Agricultural Loans</p>
                     <strong>Step 4</strong>
-                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate veritatis asperiores sint autem, est minus error hic similique deleniti assumenda rerum enim, quibusdam alias eveniet. </p>
+                    <p></p>
                 </div>
             </Container>
         </div>
