b839f5a1ba4557f5da221a7e3689852d40fee42b
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-22 21:17:20 +0100

    Footer component created and update

--- src/components/Footernew.js
+++ src/components/Footernew.js
@@ -3,7 +3,37 @@ import {Footer} from 'react-bootstrap'
 
 const Footernew = () => {
     return (
-   <div></div>
+    <div style={{height:300,backgroundColor:'green', color:'white', padding:'3em'}}>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6">
+                    <h4 style={{ color:'white'}}>A product of team wenovate</h4>
+                </div>
+                <div className="col-sm-6">
+                    <ul style={{ color:'white', listStyleType:'none'}}>
+                        <li style={{ color:'white'}}>communities</li>
+                        <li style={{ color:'white'}}>About Us</li>
+                        <li style={{ color:'white'}}>FAQs</li>
+                        <li style={{ color:'white'}}>Mission</li>
+                        <li style={{ color:'white'}}>Testimonies</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12" style={{textAlign:'center', color:'white'} }>
+                    &copy; Team Wenovate for SDG 2020
+                </div>
+            </div>
+        </div>
+        {/* <div className="container">
+            <div className="row">
+                <div className="col-md">buttom side</div>
+            </div>
+        </div> */}
+    </div>
+    
     )
 }
 
