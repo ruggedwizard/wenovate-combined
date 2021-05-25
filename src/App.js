f3d1c068888f53b7001dce3554835702c1cbfbfb
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-05-25 11:59:08 +0100

    admin panel components and layout structured

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import Navigation from './components/Navigation';
 import Footernew from './components/Footernew';
 import Bodynew from './components/Bodynew'
 import Admin from './components/Admin';
+import Blog from './components/Blog'
 import {BrowserRouter as  Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/Index'
 import Connect from './pages/Connect/index'
-
-
-// import NavLinks from './components/NavLinks';
 
 
 function App() {
   return (
     <>
       <Navigation/>
-      
-       <Connect/>
+       <Admin/>
       <Footernew/>
     </>
   );
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,9 +2,31 @@ import React from 'react'
 
 const Admin = () => {
     return (
-        <div className='container row'>
-            <h1 className='col-md-4'>sidebar</h1>
-            <h1 className='col-md-8'>main component</h1>
+        <div style={{margin:'1em'}}>
+            <div className="row" style={{marginLeft:'1em'}}>
+                <div className="jumbotron col-sm-6" style={{marginRight:'1em'}}>
+                    <h6 style={{textAlign:'center'}}>👋 Welcome Admin </h6>
+                </div>
+                <div className="jumbotron col-sm-5" style={{marginRight:'1em'}}>
+                    <h6> You Have No New Updates</h6>
+                </div>
+            </div>
+            <div style={{marginLeft:'1em'}} className="row">
+                <div className="jumbotron col-sm-4" style={{marginRight:'1em'}}>
+                    <h6>Create A New Blog Post</h6>
+                </div>
+                <div className="jumbotron col-sm-7" style={{marginRight:'1em'}}>
+                    <h6>Create A New Blog Post</h6>
+                </div>
+            </div>
+            <div style={{marginLeft:'1em'}} className="row">
+                <div className="jumbotron col-sm-4" >
+                    <h6>Create A New Blog Post</h6>
+                </div>
+                <div className="jumbotron col-sm-7" >
+                    <h6>Create A New Blog Post</h6>
+                </div>
+            </div>
         </div>
     )
 }
