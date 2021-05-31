45664e23ad9d087b32f2a1e92d53fb1b663c79a9
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-05-31 20:46:00 +0100

    Responsive feature page added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,26 @@
-import React, {Fragment} from 'react'
+import React from 'react'
+import {Container } from 'react-bootstrap'
 import farmers from '../assets/farmerstalk.jpg'
 import farmertwo from '../assets/farmer3.jpg'
-import {Box, Flex, Image, Text} from "@chakra-ui/react"
+import {Box, Image, Text} from "@chakra-ui/react"
 
 
 const Body = () => {
     return (
-        <Fragment>
-            <Box mt={70} mb={50} ml='20%'>
-                <Text fontSize="15px" color='#070941' fontWeight="bold" >Connect, make sales and learn</Text>
-                <Text numberOfLines={2} fontSize="35px" fontWeight="bold"  color='#070941'>In the <span style={{color:"#68F195"}}>best</span> online <br/> Community for <span style={{color:"#68F195"}}>farmers</span></Text>
+        <Container className="d-flex flex-column justify-content-center align-items-center m-auto">
+            <Box mt={70} ml="0" mb={50}>
+                <Text fontSize="15px"  color='#070941' fontWeight="bold" >Connect, make sales and learn</Text>
+                <Text fontSize="35px" fontWeight="bold"  color='#070941'>In the <span style={{color:"#68F195"}}>best</span> online Community for <span style={{color:"#68F195"}}>farmers</span></Text>
             </Box>    
-                <Box>
-                    <Flex >
+                <div>
+                    <div className="d-flex justify-content-space-between feature">
                             
-                        <Flex flexDirection="column" mt={25} marginLeft="20%" >
-                            <Text fontSize="25px" fontWeight="bold" color='#070941' mb={10}>Access to quality <br/> information</Text>
-                            <Text fontSize="20px" fontWeight="bold" color='#ACABB2'>About farming best<br/> practises to boost your<br/> farm production</Text>
-                        </Flex>
+                        <div className="d-flex flex-column word" style={{width:"400px", padding:'20px', marginTop:"25px"}} >
+                            <p style={{fontSize:"25px", fontWeight:"bold", color:"#070491", marginBottom:"10px"}}>Access to quality information</p>
+                            <p style={{fontSize:"20px", fontWeight:"bold", color:"#ACABB2"}}>About farming best practises to boost you farm production</p>
+                        </div>
                         <Image
-                            marginLeft="10%"
+                            className="img"
                             borderRadius="full"
                             boxSize="150px"
                             border="10px"
@@ -28,10 +29,10 @@ const Body = () => {
                             width="400px"
                             alt="img"
                             />
-                    </Flex>
-                    <Flex marginTop="50px">
+                    </div>
+                    <div className="d-flex feature" style={{marginTop:"50px"}}>
                         <Image
-                            marginLeft="20%"
+                            className="img"
                             borderRadius="full"
                             boxSize="150px"
                             border="10px"
@@ -40,14 +41,14 @@ const Body = () => {
                             width="400px"
                             alt="img"
                             />
-                            <Flex flexDirection="column" mt={25} marginLeft="10%">
-                                <Text  fontSize="25px" fontWeight="bold" color='#070941' mb={10}>A great platform <br/> to make sales</Text>
-                                <Text fontSize="20px" fontWeight="bold" color='#ACABB2'>And make valuable <br/> connection with other <br/> farmers and investors</Text>
-                            </Flex>
-                    </Flex>
-                </Box>
+                            <div className="word d-flex flex-column" style={{width:"400px", marginTop:"25px", padding:'20px'}}>
+                                <p style={{fontSize:"25px", fontWeight:"bold", color:"#070491", marginBottom:"10px"}}>A great platform to make sales</p>
+                                <p style={{fontSize:"20px", fontWeight:"bold", color:"#ACABB2"}}>And make valuable connection with other farmers and investors</p>
+                            </div>
+                    </div>
+                </div>
 
-        </Fragment>
+        </Container>
     )
 }
 
