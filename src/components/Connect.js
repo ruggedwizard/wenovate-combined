57a083c58cfd6ea54f53dcd351c899e2e1175c6d
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-05-31 20:46:52 +0100

    Connect page updated

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,6 +1,8 @@
+import {useState} from 'react'
 import { Button, Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Story from "./Story"
+import LearnMore from "./LearnMore"
 import ConnectNav from './ConnectNav/ConnectNav'
 import Connections from './ConnectNav/Connections'
 import People from './ConnectNav/People'
@@ -9,20 +11,24 @@ import SupplyChain from './ConnectNav/SupplyChain'
 
 
 const Connect = ()=>{
+
+    const [display, setDisplay] = useState(false)
+
     return (
         <Router>
             <Container>
                 <div className='connect-text col-sm-6 col-lg-8 mb-4' >
                     <h1>Introducing <span className='green-text'>connect,</span> an easy way to reach <span className='green-text'>people</span> accross farming communities.</h1>
                 </div>
-                <Button variant='primary' style={{marginLeft:"15px"}}>Learn more</Button> 
+                <Button variant='primary' style={{marginLeft:"15px"}} onClick={() => setDisplay(!display)}>{display ? "Show less" : "Learn more"}</Button> 
+               { display && <LearnMore />}
                 <Story/>
                 <ConnectNav />
             
-                    <Route exact path='/connections' component={Connections} />
-                    <Route path='/people' component={People} />
-                    <Route path='/market' component={MarketPlace} />
-                    <Route path='/supply' component={SupplyChain} />
+                    <Route exact path='/connect/connections' component={Connections} />
+                    <Route path='/connect/people' component={People} />
+                    <Route path='/connect/market' component={MarketPlace} />
+                    <Route path='/connect/supply' component={SupplyChain} />
          
             </Container>
         </Router>
