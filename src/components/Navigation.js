fa903d7fe936c5f22f9acf2d0f9f9d4c92994f9c
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-05-31 20:45:19 +0100

    Navbar responsiveness added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar, Container } from 'react-bootstrap'
 import Registermodal from './Registermodal'
 import Loginmodal from './Loginmodal'
 import { Link } from 'react-router-dom';
@@ -11,8 +11,8 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return(
-        <>
-            <Navbar expand='md' >
+        <Container>
+            <Navbar expand='md' sticky="top" style={{backgroundColor:"white"}}>
                 <Navbar.Brand style={{marginRight:60, marginLeft:5}}>
                     <Link to="/" >
                         <div className='cirle-icon'></div>
@@ -21,9 +21,9 @@ const Navigation = () => {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='navbar-content'/>
                 <Navbar.Collapse id='navbar-content' >
-                    <Nav className='mr-auto' style={{marginLeft:200}}>
+                    <Nav className='m-auto' style={{marginLeft:200}}>
                         <Nav.Link>
-                            <Link to="/body" ><strong style={{fontSize:"20px", marginRight:100}}>Features</strong></Link>
+                            <Link to="/body"  ><strong style={{fontSize:"20px", marginRight:100}}>Features</strong></Link>
                         </Nav.Link>
                         <Nav.Link>
                         <Link to="/blog"><strong style={{fontSize:"20px", marginRight:100}}>Blog</strong> </Link>
@@ -31,17 +31,17 @@ const Navigation = () => {
                         <Nav.Link>
                         <Link to="/connect" ><strong style={{fontSize:"20px", marginRight:100}}>Connect</strong></Link>
                         </Nav.Link>
-                        <Nav.Link style={{marginRight:100}}>
+                        <Nav.Link>
                             <Registermodal/>
                         </Nav.Link>
-                        <Nav.Link style={{marginRight:100}}>
+                        <Nav.Link>
                             <Loginmodal/>
                         </Nav.Link>
 
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-        </>
+        </Container>
     )
 }
 
