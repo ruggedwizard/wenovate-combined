2089dd00a2073685d4e8fa68cb2ba06b5b3a7d41
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-06-03 21:09:17 +0100

    Person component updated

--- src/components/ConnectNav/Person.js
+++ src/components/ConnectNav/Person.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import { div } from "react-bootstrap"
 import farmer from "../../assets/farmer3.jpg"
 
 
@@ -7,33 +6,31 @@ import farmer from "../../assets/farmer3.jpg"
 const Person = (props) => {
     const { name, category, rating, country, connections } = props.person;
     return(
-        <div>
-            <div class="col-12 col-md-6 col-lg-4 mb-5">
-                <div class="card" style={{width:"17.5em"}}>
-                    <div class="card-header text-center">
-                    <img src={farmer} alt="Brek" height="150" width="150" class="rounded-circle gradient-mint"/>
+            <div className="col-12 col-md-6 col-lg-4 mb-5">
+                <div className="card" style={{width:"17.5em"}}>
+                    <div className="card-header text-center">
+                    <img src={farmer} alt="Brek" height="150" width="150" className="rounded-circle gradient-mint"/>
                     </div>
-                    <div class="card-content">
-                        <div class="card-body text-center">
-                            <h4 class="card-title">{ name }</h4>
-                            <p class="category text-gray font-small-4">{category}</p>
-                            <hr class="grey"/>
-                            <div class="row grey">
-                                <div class="col-4">
-                                    <span><i class="ft-star mr-1"></i> <span>{rating}</span></span>
+                    <div className="card-content">
+                        <div className="card-body text-center">
+                            <h4 className="card-title">{ name }</h4>
+                            <p className="category text-gray font-small-4">{category}</p>
+                            <hr className="grey"/>
+                            <div className="row grey">
+                                <div className="col-4">
+                                    <span><i className="ft-star mr-1"></i> <span>{rating}</span></span>
                                 </div>
-                                <div class="col-4">
-                                    <span><i class="ft-globe mr-1"></i> <span>{country}</span></span>
+                                <div className="col-4">
+                                    <span><i className="ft-globe mr-1"></i> <span>{country}</span></span>
                                 </div>
-                                <div class="col-4">
-                                    <span><i class="ft-book mr-1"></i> <span>{connections}</span></span>
+                                <div className="col-4">
+                                    <span><i className="ft-book mr-1"></i> <span>{connections}</span></span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
