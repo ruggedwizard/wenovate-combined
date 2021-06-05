623a70113b272407fdbf66a321052cee86c61944
Damilare Tolulope <damilaretolulope1@gmail.com>
2021-06-05 01:46:05 +0100

    Community page updated

--- src/components/FooterLinks/Community.js
+++ src/components/FooterLinks/Community.js
@@ -1,10 +1,64 @@
 import React from 'react'
+import farmer from "../../assets/farmer3.jpg"
 import { Container } from 'react-bootstrap'
 
 const Community = () => {
     return (
         <Container>
             <h1>Community</h1>
+            <section id="content-types">
+                <div class="row">
+                    <div class="col-12 mt-3 mb-1">
+                    <div class="content-header">Community</div>
+                    <p class="content-sub-header">Cards support a wide variety of content, including images, text, list groups,
+                        links, and more. Mix and match multiple
+                        content types to create the card you need.</p>
+                    </div>
+                </div>
+
+                <div class="row match-height">
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <div class="card">
+                            <div class="card-content">
+                            <img class="card-img-top img-fluid" src={ farmer } alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title">Basic</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                <a class="btn btn-raised btn-danger">Go somewhere</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <div class="card">
+                            <div class="card-content">
+                            <img class="card-img-top img-fluid" src={ farmer } alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title">Basic</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                <a class="btn btn-raised btn-danger">Go somewhere</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <div class="card">
+                            <div class="card-content">
+                            <img class="card-img-top img-fluid" src={ farmer } alt="Card image cap" />
+                            <div class="card-body">
+                                <h4 class="card-title">Basic</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                content.</p>
+                                <a class="btn btn-raised btn-danger">Go somewhere</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                </section>
         </Container>
     )
 }
