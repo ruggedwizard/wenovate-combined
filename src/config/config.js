a2355675f560e4dc6a3f1c29d7ea0d0689c1b689
David Isaac Survive <davidisaacsurvive@gmail.com>
2021-06-12 12:11:45 +0100

    firbase connected to firebase emulator, Auth storage and firestore features added

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
-import * as firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/storage'
 import 'firebase/firestore'
 // Your web app's Firebase configuration
   var firebaseConfig = {
@@ -10,5 +11,21 @@ import 'firebase/firestore'
     messagingSenderId: "558350466008",
     appId: "1:558350466008:web:75c4fafa73878a1224d0ad"
   };
+
+  if(location.hostname === 'localhost'){
+    firebaseConfig ={
+       authDomain:'http:localhost:9099?ns=wenovate-f9487',
+       databaseURL:'http:localhost:9000?ns=wenovate-f9487',
+       storageBucket:'http:localhost:9199?ns=wenovate-f9487',
+
+    }
+  }
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  
+  const Auth = firebase.auth()
+  const ProjectStoreage = firebase.storage()
+  const ProjectFirestore = firebase.firestore()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+  
+  export {Auth, ProjectStoreage , ProjectFirestore, timestamp}
